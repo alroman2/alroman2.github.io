01a79a8d6278e2e2b46e23ce49f7e9a5f209bfd9
Alex <alex37888@gmail.com>
2020-05-21 23:19:56 -0700

    adding a row adds three columns now

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-    let row = '<div class="row"> <div class="col"> 1 <div> <div>'
+    let row = '<div class="row"> <div class="col"> first row <div><div class="col"> second row<div><div class="col">Third row<div> <div>'
     $(document).ready(function(){
         $("button").click(function(){
             $("div").append(row);
