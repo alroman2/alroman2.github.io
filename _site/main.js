c0f14d4d3ed1e94c4e45877c03a3aef037571f02
Alex <alex37888@gmail.com>
2020-08-03 04:22:59 -0700

    added new products, color palette, add to cart btn

--- _site/main.js
+++ _site/main.js
@@ -1,6 +1,6 @@
     var prodelem = $(' <img src="/data/product/imgs/pic1.jpg" />')
    $(document).ready(function(){
-        $("button").click(function(){
+        $("").click(function(){
             $('#hockey').after(prodelem);
         });
     });
