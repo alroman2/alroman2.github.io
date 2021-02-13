811bb0defc0ac744ee9f4180204a17765c4093bd
Alex <alex37888@gmail.com>
2021-02-12 17:42:45 -0800

    added mission, and product api calls

--- ShopController.js
+++ ShopController.js
@@ -22,7 +22,7 @@ function table(){
         document.getElementById('product_table').appendChild(row_div);
         this.rows++;
     }
-    this.add_cell = function (index) {
+    this.add_cell = function (index, image_string) {
         const product_div = document.createElement('div');
         const product_grid_div = document.createElement('div');
         const product_image_div = document.createElement('div');
@@ -46,7 +46,7 @@ function table(){
         button.className = 'btn btn-dark btn-rounded btn-sm mr-1 mb-2';
 
         img.className = 'pic-1';
-        img.src = '/data/product/imgs/pic1.jpg';
+        img.src = image_string;
         product_link.innerText = 'All american';
         price_span.innerHTML = '$5 </br>'
         button.innerText = 'Buy now';
@@ -62,31 +62,42 @@ function table(){
         button_span.appendChild(button);
         
         content_name_header.appendChild(product_link);
-
+        
         document.getElementById('row'+ index).appendChild(product_div);
-
+        
     }
 }
 
 function generate_content(){
-        const Products_class = Parse.Object.extend('Products');
-        const query = new Parse.Query(Products_class);
-        query.equalTo("name", "All American");
-        query.find().then((results) => {
-         // You can use the "get" method to get the value of an attribute
-        // Ex: response.get("<ATTRIBUTE_NAME>")
-        if (typeof document !== 'undefined') document.write(`ParseObjects found: ${JSON.stringify(results)}`);
-        console.log('ParseObjects found:', results);
-        }, (error) => {
-        if (typeof document !== 'undefined') document.write(`Error while fetching ParseObjects: ${JSON.stringify(error)}`);
-        console.error('Error while fetching ParseObjects', error);
-        });
-
+    
         let table_controller = new table();
         table_controller.add_row();
-        table_controller.add_row()
+        table_controller.add_row();
+        const Products_class = Parse.Object.extend('Products');
+        const Prods = new Parse.Query(Products_class);
+        let test;
+        Prods.get("iCGIoUKLvm")
+        .then((mask) => {
+            
+            test = mask.get("image");
+            console.log("finished api request")
+            console.log(test.url());
+            let image_url_string = ""+test.url();
+            //$("test1").src= image_url_string;        
+            table_controller.add_cell(0, image_url_string);
+        }, (error) => {
+
+        });
+        // query.find().then((results) => {
+        //  // You can use the "get" method to get the value of an attribute
+        // // Ex: response.get("<ATTRIBUTE_NAME>")
+        // if (typeof document !== 'undefined') document.write(`ParseObjects found: ${JSON.stringify(results)}`);
+        // console.log('ParseObjects found:', results);
+        // }, (error) => {
+        // if (typeof document !== 'undefined') document.write(`Error while fetching ParseObjects: ${JSON.stringify(error)}`);
+        // console.error('Error while fetching ParseObjects', error);
+        // });
 
 
-        table_controller.add_cell(0);
      
 }
