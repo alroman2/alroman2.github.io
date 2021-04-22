d983c7feafe1f0efefeba14f42137dfcc5ae5fc7
Alex <alex37888@gmail.com>
2021-04-22 03:38:45 -0700

    Update cart page to display item name, img,  total

--- _site/CartController.js
+++ _site/CartController.js
@@ -1,64 +1,41 @@
-/**
- * <ul class="list-group">
-  <li class="list-group-item d-flex justify-content-between align-items-center">
-    A list item
-    <span class="badge bg-primary rounded-pill">14</span>
-  </li>
-  <li class="list-group-item d-flex justify-content-between align-items-center">
-    A second list item
-    <span class="badge bg-primary rounded-pill">2</span>
-  </li>
-  <li class="list-group-item d-flex justify-content-between align-items-center">
-    A third list item
-    <span class="badge bg-primary rounded-pill">1</span>
-  </li>
-</ul>
- */
 
-function Cart(){
-    this.items;
-    this.iterator = 0;
-
-    this.construct = () => {
-        this.items = {};
-
-    }
-
-    this.addItem = (item,qty) => {
-        this.items[item] = qty; 
-    }
-
-    this.removeItem = (item, qty) => {
-        if (this.items[item] - qty <= 0){
-            delete this.items[item];
-        } else{
-            this.items[item] -= qty;
-        }
-    }
-
-}
 
 function Cell(){
     this.listItem;
     this.priceSpan;
+    this.nameText;
     this.price;
     this.name;
+    this.imgDiv;
+
     
     this.cell = (row) =>{
         this.listItem = document.createElement('li');
+        this.nameText = document.createElement('h3');   
+        this.imgDiv   = document.createElement('div');
+        this.imgSrc   = document.createElement('img');
         this.priceSpan = document.createElement('span');
         this.listItem.id = 'listItem' + row;
-        this.listItem.id = 'priceSpan' + row;
+        this.priceSpan.id = 'priceSpan' + row;
+        this.imgDiv.id = 'imgDiv' + row;
+        this.nameText.id = 'nameText' + row;
+        this.imgDiv.className = 'cart-item';
+        this.imgSrc.className = 'cart-img';
+        this.nameText.className ='cart-itemName';
         this.listItem.className = 'list-group-item d-flex justify-content-between align-items-center';
         this.priceSpan.className = 'badge bg-primary rounded-pill';
         //this.listItem.appendChild(this.priceSpan);
     }
 
-    this.setCell = (name,price) => {
-        this.listItem.innerHTML = name;
+    this.setCell = (name,price,imgURL) => {
+        this.nameText.innerText = name;
         this.price = price;
         this.priceSpan.innerHTML = '$'+price;
+
+        this.imgSrc.src = (imgURL != null) ? new URL(imgURL): "";
+        
     }
+
 
     this.setPrice = (price) => {
         this.price = price;
@@ -75,7 +52,7 @@ function table(){
     this.unorderedList;
     this.table_contents;
     this.rows;
-    this.total;
+    //this.total;
 
 
     this.construct = function(){
@@ -90,22 +67,29 @@ function table(){
         this.table_contents = [];
     }
 
-    this.add_cell = function () {
+    this.add_cell = function (cartItem) {
         let tableCell = new Cell();
         tableCell.cell(this.rows);
-        tableCell.setCell('Item'+this.rows,5.00);
+        tableCell.setCell(cartItem.product.name, cartItem.product.price, cartItem.product.img_url);
+        tableCell.imgDiv.appendChild(tableCell.imgSrc);
+        tableCell.listItem.appendChild(tableCell.imgDiv);
+        tableCell.listItem.appendChild(tableCell.nameText);
         tableCell.listItem.appendChild(tableCell.priceSpan);
         this.unorderedList.appendChild(tableCell.listItem);
         this.table_contents[this.rows] = tableCell;
         this.rows++;
-        this.total += 5.00;
+        //this.total += 5.00;
         console.log(this.table_contents.length);
     }
 
-    this.totalCell = (isActive) => {
+    this.totalCell = (isActive,totalPrice) => {
+        if (!isActive){
+            return;
+        }
         const priceTotalCell = new Cell();
         priceTotalCell.cell(this.rows);
-        priceTotalCell.setCell('Total Price: ', this.total);
+        priceTotalCell.setCell('Total Price: ', totalPrice);
+        priceTotalCell.listItem.appendChild(priceTotalCell.nameText);
         priceTotalCell.listItem.appendChild(priceTotalCell.priceSpan);
         this.unorderedList.appendChild(priceTotalCell.listItem);
         this.rows++;
@@ -134,12 +118,16 @@ function generate_cart(){
     console.log('runnn')
     let itemsTable = new table();
     itemsTable.construct();
-    for (let index = 0; index < 5; index++) {
-      itemsTable.add_cell();
+
+    
+    for (let index = 0; index < localSessionCart.cartItemCount; index++) {
+        if (localSessionCart.cartObj[index] != null){
+            itemsTable.add_cell(localSessionCart.cartObj[index]);
+        }
     }   
 
-    itemsTable.totalCell(true);
-    itemsTable.removeCell(2); 
+    itemsTable.totalCell(true, localSessionCart.cartTotal);
+    
 
-    console.log(sessionStorage);
+    console.log(JSON.parse(sessionStorage.getItem("guest_cart")));
 }
